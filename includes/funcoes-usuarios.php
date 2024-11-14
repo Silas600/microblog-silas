c9721b247f6917ab8f12bd9c00f6054794555d67
Silas600 <silasrochsmedeiros.senac@gmail.com>
2024-11-14 15:24:29 -0300

    funcoes

---
+++ includes/funcoes-usuarios.php
@@ -0,0 +1,19 @@
+<?php
+require "conecta.php";
+
+// Função para inserir novos usuários
+function inserirUsuario($conexao, $nome, $email, $senha, $tipo){
+    // Montando o comando SQL para fazer o insert dos dados 
+    $sql = "INSERT INTO usuarios (nome, email, tipo, senha)
+            VALUES('$nome', '$email', '$tipo', '$senha')";
+
+    // Executando o comando no banco via PHP
+    mysqli_query($conexao, $sql);
+
+
+
+
+
+
+    
+}
